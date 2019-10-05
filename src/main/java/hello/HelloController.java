d90c6d08bcbd664592307f442df2649fb01c32af
watercannons <xingbuqin@umail.ucsb.edu>
2019-10-05 00:44:25 -0700

    wish me luck

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,11 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        String html = "<h1>Hello World!</h1>\n" +
+            "<p>This web app is powered by \n" +
+            "<a href='https://github.com/watercannons/spring-boot-minimal-webapp'>this github repo</a></p>\n";
+            return html;
+        //return "Greetings from Spring Boot!";
     }
     
 }
